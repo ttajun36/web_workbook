68da21266ef66a0a09e7ae1f00952402f51e9fe7
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2024-11-24 18:48:30 +0900

    DB와 Java 연결. hikari, DAO, VO

---
+++ src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
@@ -0,0 +1,40 @@
+package org.zerock.jdbcex.dao;
+
+import lombok.Cleanup;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TodoDAO {
+
+    public String getTime() {
+        String now = null;
+
+        // try 안에 리소스를 집어 넣는 이유는 close()를 보장 해주기 위해서.
+        try(Connection connection = ConnectionUtil.INSTANCE.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+            ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            resultSet.next();
+
+            now = resultSet.getString(1);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return now;
+    }
+
+    public String getTime2() throws Exception {
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        String now = resultSet.getString(1);
+
+        return now;
+    }
+}
---
+++ src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -0,0 +1,20 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zerock.jdbcex.dao.TodoDAO;
+
+public class TodoDAOTests {
+
+    private TodoDAO todoDAO;
+
+    @BeforeEach
+    public void ready(){
+        todoDAO = new TodoDAO();
+    }
+
+    @Test
+    public void testTime() throws Exception{
+        System.out.println(todoDAO.getTime());
+    }
+}
