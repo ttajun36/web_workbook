675d2188776f3554906f8a44bfecf54a076e08b9
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2024-11-24 15:50:40 +0900

    test코드

---
+++ src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,15 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConnectTests {
+
+    @Test
+    public void test1(){
+        int v1 = 10;
+        int v2 = 20;
+
+        Assertions.assertEquals(v1,v2);
+    }
+}
