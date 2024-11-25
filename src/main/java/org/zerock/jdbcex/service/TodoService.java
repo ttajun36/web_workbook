eb2329b826519ae9e17e923e44ad9a9d5bd1d8ab
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2024-11-25 10:00:26 +0900

    db <-> DAO<->(VO <-> DTO) <-> WEB TodoService 구현 완료

--- src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -46,4 +46,23 @@ public enum TodoService {
 
         return dtoList;
     }
+
+    public TodoDTO get(Long tno) throws Exception{
+
+        log.info("tno: " + tno);
+        TodoVO todoVO = dao.selectOne(tno);
+        TodoDTO todoDTO = modelMapper.map(todoVO, TodoDTO.class);
+        return todoDTO;
+    }
+
+    public void remove(Long tno) throws Exception{
+        log.info("tno: " + tno);
+        dao.deleteOne(tno);
+    }
+
+    public void modify(TodoDTO todoDTO) throws Exception{
+        log.info("todoDTO: " + todoDTO);
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+        dao.updateOne(todoVO);
+    }
 }
