763f05d3505ae56e5789dc8fd025e219dff5f90f
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2024-11-26 10:10:33 +0900

    쿠키를 통해 조회 기록 저장

--- src/main/java/org/zerock/w2/controller/TodoReadController.java
+++ src/main/java/org/zerock/w2/controller/TodoReadController.java
@@ -6,6 +6,7 @@ import org.zerock.w2.service.TodoService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,10 +26,55 @@ public class TodoReadController extends HttpServlet {
             TodoDTO todoDTO = todoService.get(tno);
 
             req.setAttribute("dto", todoDTO);
+
+            //쿠키 찾기
+            Cookie viewTodoCookie = findCookie(req.getCookies(), "viewTodos");
+            String todoListStr = viewTodoCookie.getValue();
+            boolean exist = false;
+
+            //쿠키가 존재하고 현재 tno가 존재할 때
+            if(todoListStr != null && todoListStr.indexOf(tno+"-") >= 0){
+                exist = true;
+            }
+
+            log.info("exist: " + exist);
+
+            if(!exist){
+                todoListStr += tno + "-";
+                viewTodoCookie.setValue(todoListStr);
+                viewTodoCookie.setMaxAge(60* 60* 24);
+                viewTodoCookie.setPath("/");
+                resp.addCookie(viewTodoCookie);
+            }
+
             req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
         } catch(Exception e){
+            e.printStackTrace();
             log.error(e.getMessage());
             throw new ServletException("read error");
         }
+    }
+
+    //cookieName cookie가 있는지 확인하고, 없으면 발행해준다.
+    private Cookie findCookie(Cookie[] cookies, String cookieName){
+
+        Cookie targetCookie = null;
+
+        if(cookies != null && cookies.length > 0){
+            for(Cookie ck : cookies){
+                if(ck.getName().equals(cookieName)){
+                    targetCookie = ck;
+                    break;
+                }
+            }
+        }
+
+        if(targetCookie == null){
+            targetCookie = new Cookie(cookieName, "");
+            targetCookie.setPath("/");
+            targetCookie.setMaxAge(60* 60* 24);
+        }
+
+        return targetCookie;
     }
 }
