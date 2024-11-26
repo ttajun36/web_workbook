7093328f494102affb2d8205cbace8c2345602c6
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2024-11-26 11:13:28 +0900

    자동 로그인 구현: 세션과 쿠키를 동시에 이용하여

--- src/main/java/org/zerock/w2/controller/LoginController.java
+++ src/main/java/org/zerock/w2/controller/LoginController.java
@@ -6,11 +6,9 @@ import org.zerock.w2.service.MemberService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/login")
 @Log
@@ -31,8 +29,28 @@ public class LoginController extends HttpServlet {
         String mid = req.getParameter("mid");
         String mpw = req.getParameter("mpw");
 
+        String auto = req.getParameter("auto");
+        boolean rememberMe = auto != null && auto.equals("on");
+        if (rememberMe){
+            String uuid = UUID.randomUUID().toString();
+        }
+
         try{
             MemberDTO memberDTO = MemberService.INSTANCE.login(mid, mpw);
+
+            if(rememberMe){
+                String uuid = UUID.randomUUID().toString();
+
+                MemberService.INSTANCE.updateUuid(mid, uuid);
+                memberDTO.setUuid(uuid);
+
+                Cookie rememberMeCookie = new Cookie("remember-me", uuid);
+                rememberMeCookie.setMaxAge(60*60*24*7);
+                rememberMeCookie.setPath("/");
+
+                resp.addCookie(rememberMeCookie);
+            }
+
             HttpSession session = req.getSession();
             session.setAttribute("loginInfo", memberDTO);
             resp.sendRedirect("/todo/list");
